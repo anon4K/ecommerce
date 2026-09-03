13b0d2710440aacfe8edd70575672f9bd6cdede6
Michael Offordum <michaelboss500@gmail.com>
2026-09-03 15:39:46 +0100

    Add legacy STATICFILES_STORAGE for cloudinary_storage collectstatic compatibility

--- core/settings.py
+++ core/settings.py
@@ -170,6 +170,7 @@ STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
+    "STATICFILES_STORAGE": "whitenoise.storage.CompressedManifestStaticFilesStorage",
 }
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
